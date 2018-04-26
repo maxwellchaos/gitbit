e996395f5a7d0e9aee7ff7847412f07b880c81bb
falope6870 <37956625+falope6870@users.noreply.github.com>
2018-04-26 18:41:09 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace vk_bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.pictureBoxAvatar = new System.Windows.Forms.PictureBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.mass_laik = new System.Windows.Forms.Button();
+            this.egroup = new System.Windows.Forms.Button();
+            this.delprigl = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAvatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFirstName.Location = new System.Drawing.Point(217, 47);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(57, 20);
+            this.labelFirstName.TabIndex = 0;
+            this.labelFirstName.Text = "label1";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLastName.Location = new System.Drawing.Point(217, 75);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(57, 20);
+            this.labelLastName.TabIndex = 1;
+            this.labelLastName.Text = "label2";
+            // 
+            // pictureBoxAvatar
+            // 
+            this.pictureBoxAvatar.Location = new System.Drawing.Point(29, 36);
+            this.pictureBoxAvatar.Name = "pictureBoxAvatar";
+            this.pictureBoxAvatar.Size = new System.Drawing.Size(157, 169);
+            this.pictureBoxAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxAvatar.TabIndex = 2;
+            this.pictureBoxAvatar.TabStop = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(-7, 487);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(20, 20);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // mass_laik
+            // 
+            this.mass_laik.Location = new System.Drawing.Point(426, 36);
+            this.mass_laik.Name = "mass_laik";
+            this.mass_laik.Size = new System.Drawing.Size(159, 41);
+            this.mass_laik.TabIndex = 6;
+            this.mass_laik.Text = "Массовый лайкинг";
+            this.mass_laik.UseVisualStyleBackColor = true;
+            this.mass_laik.Click += new System.EventHandler(this.mass_laik_Click);
+            // 
+            // egroup
+            // 
+            this.egroup.Location = new System.Drawing.Point(426, 101);
+            this.egroup.Name = "egroup";
+            this.egroup.Size = new System.Drawing.Size(159, 41);
+            this.egroup.TabIndex = 7;
+            this.egroup.Text = "Выход и вступление в группы";
+            this.egroup.UseVisualStyleBackColor = true;
+            // 
+            // delprigl
+            // 
+            this.delprigl.Location = new System.Drawing.Point(426, 164);
+            this.delprigl.Name = "delprigl";
+            this.delprigl.Size = new System.Drawing.Size(159, 41);
+            this.delprigl.TabIndex = 8;
+            this.delprigl.Text = " Удаление всех приглашений";
+            this.delprigl.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 232);
+            this.Controls.Add(this.delprigl);
+            this.Controls.Add(this.egroup);
+            this.Controls.Add(this.mass_laik);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.pictureBoxAvatar);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAvatar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.PictureBox pictureBoxAvatar;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button mass_laik;
+        private System.Windows.Forms.Button egroup;
+        private System.Windows.Forms.Button delprigl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace vk_bot
+{
+    public partial class Form1 : Form
+    {
+        string access_token;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Dock = DockStyle.Fill;
+            webBrowser1.Navigate("https://oauth.vk.com/authorize?client_id=6382696&scope=friends,photos,audio,video,docs,notes,pages,status,offers,questions,wall,groups,messages,notifications,stats,ads,market,offline&redirect_uri=https://api.vk.com/blank.html&display=page&response_type=token");
+            mass_laik.Visible = false;
+            egroup.Visible = false;
+            delprigl.Visible = false;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string url = e.Url.ToString();
+            if (url.Contains("error"))
+            {
+                MessageBox.Show("Ошибка");
+            }
+            if (url.Contains("access_token"))
+            {
+                //Выделяю access_token
+                int IndexAccTok = url.IndexOf("access_token");
+                access_token = url.Remove(0, IndexAccTok + 13);
+                int IndexAmp = access_token.IndexOf("&");
+                access_token = access_token.Remove(IndexAmp);
+
+                //Зпрашиваю информацию о пользователе
+                //Создаю XML документ
+                XmlDocument doc = new XmlDocument();
+                doc.Load("https://api.vk.com/method/users.get.xml?fields=photo_100&access_token=" + access_token + "&v=5.73");
+                XmlNode response = doc.SelectSingleNode("response");
+                XmlNode user = response.SelectSingleNode("user");
+
+                XmlNode FirstName = user.SelectSingleNode("first_name");
+                labelFirstName.Text = FirstName.InnerText;
+
+                XmlNode LastName = user.SelectSingleNode("last_name");
+                labelLastName.Text = LastName.InnerText;
+
+                pictureBoxAvatar.ImageLocation = user.SelectSingleNode("photo_100").InnerText;
+                webBrowser1.Visible = false;
+                mass_laik.Visible = true;
+                egroup.Visible = true;
+                delprigl.Visible = true;
+            }
+
+        }
+
+        private void mass_laik_Click(object sender, EventArgs e)
+        {
+            wallskanform newForm = new wallskanform();
+            newForm.access_token = access_token;
+            newForm.Show();
+        }
+
+        
+
+    }
+
+}
