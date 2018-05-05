9d0beea78763807d8173381efb80735abc6800c1
falope6870 <37956625+falope6870@users.noreply.github.com>
2018-05-05 17:08:25 +0300

    Апдейт 5 мая
    Доделал функцию автолайкинга

--- vk_bot/vk_bot/wallskanform.cs
+++ vk_bot/vk_bot/wallskanform.cs
@@ -14,7 +14,7 @@ namespace vk_bot
     public partial class wallskanform : Form
     {
         //создаю переменные
-        public string textbox;
+        public string textboxer;
         public string access_token;
         public string idpostlist;
         public string id2postlist;
@@ -27,7 +27,7 @@ namespace vk_bot
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textbox = textBox1.Text;
+            textboxer = textBox1.Text;
 
             webBrowser1.Navigate("https://api.vk.com/method/wall.get.xml?&owner_id=-" + textBox1.Text + "&access_token=" + access_token + "&v=5.73");
         }
@@ -48,28 +48,25 @@ namespace vk_bot
                 int IndexAmp = access_token.IndexOf("&");
                 access_token = access_token.Remove(IndexAmp);
             }
-                //добираюсь до id
-                XmlDocument tags = new XmlDocument();
-                tags.Load("https://api.vk.com/method/wall.get.xml?&owner_id=-" + textBox1.Text + "&access_token=" + access_token + "&v=5.73");
-                XmlNode response = tags.SelectSingleNode("response");
-                XmlNode tager = response.SelectSingleNode("items");
-                
 
-                foreach(XmlNode postid in tager.SelectNodes("id"))
-                {
-                   
-                    tags.Load("https://api.vk.com/method/wall.get.xml?&owner_id=-" + textBox1.Text + "&access_token=" + access_token + "&v=5.73");
+            //добираюсь до id
+            XmlDocument tags = new XmlDocument();
+            tags.Load("https://api.vk.com/method/wall.get.xml?&owner_id=-" + textBox1.Text + "&access_token=" + access_token + "&v=5.73");
+            XmlNode response = tags.SelectSingleNode("response");
+            XmlNode tager = response.SelectSingleNode("items");
 
-                    textbox = postid.InnerXml;
+            foreach(  XmlNode utag in tager.SelectNodes("post") )
+            {
+                XmlNode postid = utag.SelectSingleNode("id");
+                XmlDocument dopdoo = new XmlDocument();
 
-                    XmlDocument dopdoo = new XmlDocument();
+                dopdoo.Load("https://api.vk.com/method/likes.add.xml?type=post&owner_id=-" + textBox1.Text + "&item_id=" + textboxer + "&access_token=" + access_token + "&v=5.73");
 
-                    dopdoo.Load("https://api.vk.com/method/likes.add.xml?type=post&owner_id=-" + textBox1.Text + "&item_id=" + textbox + "&access_token=" + access_token + "&v=5.73");
+                textboxer = postid.InnerXml;
 
-                    Thread.Sleep(1000);
-                    Application.DoEvents();
-                    
-                }
+                Thread.Sleep(300);
+                Application.DoEvents();
+            }
 
                 //считаю кол-во
                 tags.Load("https://api.vk.com/method/wall.get.xml?&owner_id=-" + textBox1.Text + "&access_token=" + access_token + "&v=5.73");
@@ -82,7 +79,7 @@ namespace vk_bot
 
         private void button2_Click(object sender, EventArgs e)
         {
-            webBrowser1.Navigate("https://api.vk.com/method/likes.add.xml?type=post&owner_id=-" + textBox1.Text + "&item_id=" + textbox + "&access_token=" + access_token + "&v=5.73");
+            webBrowser1.Navigate("https://api.vk.com/method/likes.add.xml?type=post&owner_id=-" + textBox1.Text + "&item_id=" + textboxer + "&access_token=" + access_token + "&v=5.73");
         }
 
     }
