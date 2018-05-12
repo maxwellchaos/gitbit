2aa9ebc2ce38b83d26045dac8c68e0e241ab1727
falope6870 <37956625+falope6870@users.noreply.github.com>
2018-05-12 17:20:25 +0300

    ГОТОВО 12 05 18
    СДЕЛАЛ ВСЁ, ДОБАВИЛ СЧЁТЧИКИ

--- vk_bot/vk_bot/prigl.cs
+++ vk_bot/vk_bot/prigl.cs
@@ -9,13 +9,13 @@ using System.Windows.Forms;
 using System.Xml;
 using System.Threading;
 
-
 namespace vk_bot
 {
     public partial class prigl : Form
     {
         public string access_token;
         public string indexer;
+        public string errors;
 
         public prigl()
         {
@@ -24,25 +24,6 @@ namespace vk_bot
 
         private void button1_Click(object sender, EventArgs e)
         {
-            webBrowser1.Navigate("https://api.vk.com/method/groups.getInvitedUsers.xml?group_id=" + textBox1.Text + "&access_token=" + access_token + "&v=5.73");
-        }
-
-        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
-            string url = e.Url.ToString();
-            if (url.Contains("error"))
-            {
-                MessageBox.Show("Ошибка");
-            }
-            if (url.Contains("access_token"))
-            {
-                //Выделяю access_token
-                int IndexAccTok = url.IndexOf("access_token");
-                access_token = url.Remove(0, IndexAccTok + 13);
-                int IndexAmp = access_token.IndexOf("&");
-                access_token = access_token.Remove(IndexAmp);
-            }
-
             //добираюсь до индентификаторов
             XmlDocument doo = new XmlDocument();
             doo.Load("https://api.vk.com/method/groups.getInvitedUsers.xml?group_id=" + textBox1.Text + "&access_token=" + access_token + "&v=5.73");
@@ -51,23 +32,36 @@ namespace vk_bot
 
             foreach (XmlNode prof in tager.SelectNodes("user"))
             {
-                doo.Load("https://api.vk.com/method/groups.removeUser.xml?user_id=" + indexer + "&group_id=" + textBox1.Text + "&access_token=" + access_token + "&v=5.73");
                 XmlNode id = prof.SelectSingleNode("id");
                 indexer = id.InnerXml;
-
-                Thread.Sleep(220);
-                Application.DoEvents();
+                doo.Load("https://api.vk.com/method/groups.removeUser.xml?user_id=" + indexer + "&group_id=" + textBox1.Text + "&access_token=" + access_token + "&v=5.73");
+                
+                
+                //ищу ошибки
+                if (doo.InnerXml.Contains("error"))
+                {
+                    errors += 1;
+                    label3.Text = errors;
+                }
             }
+
+            label4.Text = "ГОТОВО!";
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+          
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            webBrowser1.Navigate("https://api.vk.com/method/groups.removeUser.xml?user_id="+indexer+"&group_id="+textBox1.Text+"&access_token=" + access_token + "&v=5.73");
+            
         }
 
         private void prigl_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Инструкция: 1. Нажмите на кнопку ВЫПОЛНИТЬ ЗАПРОС | 2. Нажмите на кнопку НАЧАТЬ УДАЛЯТЬ ПРИГЛАШЕНИЯ");
+            
         }
 
     }
