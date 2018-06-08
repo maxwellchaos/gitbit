55d2590ab39d0a337655bba398b4f8cc7ecdceeb
maxwellchaos <maxwellchaos@gmail.com>
2018-06-08 17:08:11 +0300

    Коррекция 2

---
+++ vk_bot/vk_bot/url.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace vk_bot
+{
+    partial class url
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textbox1
+            // 
+            this.textbox1.Location = new System.Drawing.Point(12, 41);
+            this.textbox1.Multiline = true;
+            this.textbox1.Name = "textbox1";
+            this.textbox1.Size = new System.Drawing.Size(938, 27);
+            this.textbox1.TabIndex = 0;
+            this.textbox1.Text = "166275973";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Вставте id сообщёства";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(32, 123);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(909, 406);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Найдено комментарев";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Начать удалять ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // url
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 579);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textbox1);
+            this.Name = "url";
+            this.Text = "url";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textbox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ vk_bot/vk_bot/url.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Threading;
+
+namespace vk_bot
+{
+    public partial class url : Form
+    {
+        public string access_token;
+        public string textbox;
+        public string ddos;
+        public string idcommentlist;
+        public string delscore;
+        public string textid;
+
+        public url()
+        {
+
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+
+
+            XmlDocument gg = new XmlDocument();
+            gg.Load("https://api.vk.com/method/wall.get.xml?owner_id=-" + textbox1.Text + "&access_token=" + access_token + "&v=5.73");
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument tags = new XmlDocument();
+            textbox = textbox1.Text;
+            tags.Load("https://api.vk.com/method/wall.get.xml?owner_id=-" + textbox + "&access_token=" + access_token + "&v=5.73");
+            XmlNode response = tags.SelectSingleNode("response");
+            XmlNode tager = response.SelectSingleNode("items");
+            foreach (XmlNode post in tager.SelectNodes("post"))
+            {
+
+                XmlNode id = post.SelectSingleNode("id");
+                ddos = id.InnerXml;
+
+                XmlDocument comnt = new XmlDocument();
+                comnt.Load("https://api.vk.com/method/wall.getComments.xml?owner_id=-" + textbox1.Text + "&post_id=" + ddos + "&access_token=" + access_token + "&v=5.73");
+
+                //делаю паузу
+                Thread.Sleep(500);
+                Application.DoEvents();
+
+                XmlNode response2 = comnt.SelectSingleNode("response");
+                XmlNode tager2 = response2.SelectSingleNode("count");
+                XmlNode post2 = tager2.SelectSingleNode("comment");
+                XmlNode id2 = response2.SelectSingleNode("items");
+
+                //делаю паузу
+                Thread.Sleep(500);
+                Application.DoEvents();
+
+
+                foreach (XmlNode utag2 in id2.SelectNodes("comment"))
+                {
+                    XmlDocument vopros = new XmlDocument();
+                    XmlNode who = utag2.SelectSingleNode("id");
+                    XmlNode http = utag2.SelectSingleNode("text");
+
+
+                    vopros.Load("https://api.vk.com/method/wall.deleteComment.xml?owner_id=-" + textbox1.Text + "&comment_id=" + who.InnerText + "&access_token=" + access_token + "&v=5.73");
+
+                    //делаю паузу
+                    Thread.Sleep(500);
+                    Application.DoEvents();
+                }
+            }
+        }
+    }
+}
+           
+        
+    
